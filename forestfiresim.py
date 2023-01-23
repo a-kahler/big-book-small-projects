0798e445d13b6f6de2fa7bd679fed681a696bbdb
a-kahler <austinkahler@proton.me>
2023-01-23 16:41:18 -0500

    Add main program loop

--- forestfiresim.py
+++ forestfiresim.py
@@ -31,3 +31,47 @@ FIRE_CHANCE = 0.01 # Chance a tree is hit by lightning & burns.
 PAUSE_LENGTH = 0.5
 
 
+def main():
+    forest = createNewForest()
+    bext.clear()
+
+    while True: # Main program loop
+        displayForest(forest)
+
+        # Run a single simulation step:
+        nextForest = {'width': forest['width'],
+                      'height': forest['height']}
+        
+        for x in range(forest['width']):
+            for y in range(forest['height']):
+                if (x, y) in nextForest:
+                    # If we've already set nextForest[(x, y)] on a
+                    # previous iteration, just do nothing here:
+                    continue
+
+                if ((forest[(x, y)] == EMPTY)
+                    and (random.random() <= GROW_CHANCE)):
+                    # Grow a tree in this empty space.
+                    nextForest[(x, y)] = TREE
+                elif ((forest[(x, y)] == TREE)
+                    and (random.random() <= FIRE_CHANCE)):
+                    # Lightning sets this tree on fire.
+                    nextForest[(x, y)] = FIRE
+                elif forest[(x, y)] == FIRE:
+                    # This tree is currently burning.
+                    # Loop through all the neighboring spaces:
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
+                            # Fire spreads to neighboring trees:
+                            if forest.get((x + ix, y + iy)) == TREE:
+                                nextForest[(x + ix, y + iy)] = FIRE
+                    # The tree has burned down now, so erase it:
+                    nextForest[(x, y)] = forest[(x, y)]
+                else:
+                    # Just copy the existing object:
+                    nextForest[(x, y)] = forest[(x, y)]
+        forest = nextForest
+
+        time.sleep(PAUSE_LENGTH)
+
+
