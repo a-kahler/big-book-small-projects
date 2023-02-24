4e98c24792ad578eb975cba9f60ab1d042b3c83e
a-kahler <austinkahler@proton.me>
2023-02-24 02:59:26 -0500

    Adding in lines to load in WORDS list

--- hacking.py
+++ hacking.py
@@ -12,3 +12,12 @@ import random, sys
 # Set up the constants:
 # The garbage filler characters for the "computer memory" display.
 GARBAGE_CHARS = '~!@#$%^&*()_+-={}[]|;:,.<>?/'
+
+# Load the WORDS list from a text file that has 7-letter words.
+with open('sevenletterwords.txt') as wordListFile:
+    WORDS = wordListFile.readlines()
+for i in range(len(WORDS)):
+    # Convert each word to uppercase and remove the trailing newline:
+    WORDS[i] = WORDS[i].strip().upper()
+
+
