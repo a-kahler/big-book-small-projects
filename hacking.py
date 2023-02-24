6e98a75de94cd6b5d6e902b8a8bf28045f837cd6
a-kahler <austinkahler@proton.me>
2023-02-24 02:56:08 -0500

    Add constants

--- hacking.py
+++ hacking.py
@@ -7,3 +7,8 @@ word is the password by using clues each guess gives you.
 # NOTE: This program requires the sevenletterwords.txt file. You can
 # download it from https://inventwithpython.com/sevenletterwords.txt
 
+import random, sys
+
+# Set up the constants:
+# The garbage filler characters for the "computer memory" display.
+GARBAGE_CHARS = '~!@#$%^&*()_+-={}[]|;:,.<>?/'
