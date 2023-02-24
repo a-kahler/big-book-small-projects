78c66ef6cce6ffcb83a2c0a6c2d815e7d24a8ee1
a-kahler <austinkahler@proton.me>
2023-02-24 02:54:20 -0500

    Initial commit

---
+++ hacking.py
@@ -0,0 +1,9 @@
+"""Hacking Minigame, by Austii KAY
+The hacking mini-game from "Fallout 3". Find out which seven-letter
+word is the password by using clues each guess gives you.
+
+"""
+
+# NOTE: This program requires the sevenletterwords.txt file. You can
+# download it from https://inventwithpython.com/sevenletterwords.txt
+
