ca3de493af51487b6d0403b66d10a3dbca31aac8
a-kahler <austinkahler@proton.me>
2023-03-15 14:29:32 -0400

    add main game loop

--- hungryrobots.py
+++ hungryrobots.py
@@ -22,3 +22,40 @@ DEAD_ROBOT = 'X'
 WALL = chr(9617)
 
 
+def main():
+    print('''Hungry Robots, by Austii KAY
+    
+You are trapped in a maze with hungry robots! You don't know why robots
+need to eat, but you don't want to find out. The robots are badly
+programmed and will move directly toward you, even if blocked by walls.
+You must trick the robots into crashing into each other (or dead robots)
+without being caught. You have a personal teleporter device, but it only
+has enough battery for {} trips. Keep in mind, you and robots can slip
+through the corners of two diagonal walls!
+'''.format(NUM_TELEPORTS))
+
+    input('Press Enter to begin...')
+
+    # Set up a new game:
+    board = getNewBoard()
+    robots = addRobots(board)
+    playerPosition = getRandomEmptySpace(board, robots)
+    while True: # Main game loop.
+        displayBoard(board, robots, playerPosition)
+
+        if len(robots) == 0: # Check if the player has won.
+            print('All the robots have crashed into each other and you')
+            print('lived to tell the tale! Good job!')
+            sys.exit()
+
+        # Move the player and robots:
+        playerPosition = askForPlayerMove(board, robots, playerPosition)
+        robots = moveRobots(board, robots, playerPosition)
+
+        for x, y in robots: # Check if the player has lost.
+            if (x, y) == playerPosition:
+                displayBoard(board, robots, playerPosition)
+                print('You have been caught by a robot!')
+                sys.exit()
+
+
