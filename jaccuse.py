f577765611b1339600078ead029f9bb22e0e1bac
a-kahler <austinkahler@proton.me>
2023-04-08 21:18:45 -0400

    initial commit

---
+++ jaccuse.py
@@ -0,0 +1,11 @@
+"""J'ACCUSE!, by Austii KAY
+A mystery game of intrigue and a missing cat
+
+"""
+
+# Play the original Flash game at:
+# https://homestarrunner.com/videlectrix/wheresanegg.html
+# More info at: http://www.hrwiki.org/wiki/Where's_an_Egg%3F
+
+import time, random, sys
+
