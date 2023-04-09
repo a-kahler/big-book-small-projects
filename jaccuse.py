f4a53a8cb50610ba76f1dea552ede19d3738e28f
a-kahler <austinkahler@proton.me>
2023-04-08 22:40:27 -0400

    final commit & push

--- jaccuse.py
+++ jaccuse.py
@@ -154,3 +154,142 @@ for interviewee in random.sample(SUSPECTS, random.randint(3, 4)):
 #print('culprit =', culprit)
 
 # START OF THE GAME
+print("""J'ACCUSE! (a mystery game)")
+By Austii KAY
+Inspired by Homestar Runner\'s "Where\'s an Egg?" game
+
+You are the world-famous detective Mathilde Camus.
+ZOPHIE THE CAT has gone missing, and you must sift through the clues.
+Suspects either always tell lies, or always tell the truth. Ask them
+about other people, places, and items to see if the details they give are
+truthful and consistent with your observations. Then you will know if
+their clue about ZOPHIE THE CAT is true or not. Will you find ZOPHIE THE
+CAT in time and accuse the guilty party?
+""")
+input('Press Enter to begin...')
+
+
+startTime = time.time()
+endTime = startTime + TIME_TO_SOLVE
+
+while True: # Main game loop.
+    if time.time() > endTime or accusationsLeft == 0:
+        # Handle "game over" condition:
+        if time.time() > endTime:
+            print('You have run out of time!')
+        elif accusationsLeft == 0:
+            print('You have accused too many innocent people!')
+        culpritIndex = SUSPECTS.index(culprit)
+        print('It was {} at the {} with the {} who catnapped her!'.format(culprit, PLACES[culpritIndex], ITEMS[culpritIndex]))
+        print('Better luck next time, Detective.')
+        sys.exit()
+    
+    print()
+    minutesLeft = int(endTime - time.time()) // 60
+    secondsLeft = int(endTime - time.time()) % 60
+    print('Time left: {} min, {} sec'.format(minutesLeft, secondsLeft))
+
+    if currentLocation == 'TAXI':
+        print('  You are in your TAXI. Where do you want to go?')
+        for place in sorted(PLACES):
+            placeInfo = ''
+            if place in visitedPlaces:
+                placeInfo = visitedPlaces[place]
+            nameLabel = '(' + place[0] + ')' + place[1:]
+            spacing = " " * (LONGEST_PLACE_NAME_LENGTH - len(place))
+            print('{} {}{}'.format(nameLabel, spacing, placeInfo))
+        print('(Q)UIT GAME')
+        while True: # Keep asking until a valid response is given.
+            response = input('> ').upper()
+            if response == '':
+                continue # Ask again.
+            if response == 'Q':
+                print('Thanks for playing!')
+                sys.exit()
+            if response in PLACE_FIRST_LETTERS.keys():
+                break
+        currentLocation = PLACE_FIRST_LETTERS[response]
+        continue # Go back to the start of the main game loop.
+
+    # At a place; player can ask for clues.
+    print('  You are at the {}.'.format(currentLocation))
+    currentLocationIndex = PLACES.index(currentLocation)
+    thePersonHere = SUSPECTS[currentLocationIndex]
+    theItemHere = ITEMS[currentLocationIndex]
+    print(' {} with the {} is here.'.format(thePersonHere, theItemHere))
+
+    # Add the suspect and item at this place to our list of known
+    # suspects and items:
+    if thePersonHere not in knownSuspectsAndItems:
+        knownSuspectsAndItems.append(thePersonHere)
+    if ITEMS[currentLocationIndex] not in knownSuspectsAndItems:
+        knownSuspectsAndItems.append(ITEMS[currentLocationIndex])
+    if currentLocation not in visitedPlaces.keys():
+        visitedPlaces[currentLocation] = '({}, {})'.format(thePersonHere.lower(), theItemHere.lower())
+
+    # If the player has accused this person wrongly before, they
+    # won't give clues:
+    if thePersonHere in accusedSuspects:
+        print('They are offended that you accused them,')
+        print('and will not help with your investigation.')
+        print('You go back to your TAXI.')
+        print()
+        input('Press Enter to continue...')
+        currentLocation = 'TAXI'
+        continue # Go back to the start of the main game loop.
+
+    # Display menu of known suspects & items to ask about:
+    print()
+    print('(J) "J\'ACCUSE!" ({} accusations left)'.format(accusationsLeft))
+    print('(Z) Ask if they know where ZOPHIE THE CAT is.')
+    print('(T) Go back to the TAXI.')
+    for i, suspectOrItem in enumerate(knownSuspectsAndItems):
+        print('({}) Ask about {}'.format(i + 1, suspectOrItem))
+    
+    while True: # Keep asking until a valid response is given.
+        response = input('> ').upper()
+        if response in 'JZT' or (response.isdecimal() and 0 < int(response) <= len(knownSuspectsAndItems)):
+            break
+    
+    if response == 'J': # Player accuses this suspect.
+        accusationsLeft -= 1 # Use up an accusation.
+        if thePersonHere == culprit:
+            # You've accused the correct suspect.
+            print('You\'ve cracked the case, Detective!')
+            print('It was {} who catnapped ZOPHIE THE CAT.'.format(culprit))
+            minutesTaken = int(time.time() - startTime) // 60
+            secondsTaken = int(time.time() - startTime) % 60
+            print('Good job! You solved it in {} min, {} sec.'.format(minutesTaken, secondsTaken))
+            sys.exit()
+        else:
+            # You've accused the wrong suspect.
+            accusedSuspects.append(thePersonHere)
+            print('You have accused the wrong person, Detective!')
+            print('They will not help you with anymore clues.')
+            print('You go back to your TAXI.')
+            currentLocation = 'TAXI'
+
+    elif response == 'Z': # Player asks about Zophie.
+        if thePersonHere not in zophieClues:
+            print('"I don\'t know anything about ZOPHIE THE CAT."')
+        elif thePersonHere in zophieClues:
+            print('   They give you this clue: "{}"'.format(zophieClues[thePersonHere]))
+            # Add non-place clues to the list of known things:
+            if zophieClues[thePersonHere] not in knownSuspectsAndItems and zophieClues[thePersonHere] not in PLACES:
+                knownSuspectsAndItems.append(zophieClues[thePersonHere])
+
+    elif response == 'T': # Player goes back to the taxi.
+        currentLocation = 'TAXI'
+        continue # Go back to the start of the main game loop.
+
+    else: # Player asks about a suspect or item.
+        thingBeingAskedAbout = knownSuspectsAndItems[int(response) - 1]
+        if thingBeingAskedAbout in (thePersonHere, theItemHere):
+            print('  They give you this clue: "No comment."')
+        else:
+            print('  They give you this clue: "{}"'.format(clues[thePersonHere][thingBeingAskedAbout]))
+            # Add non-place clues to the list of known things:
+            if clues[thePersonHere][thingBeingAskedAbout] not in knownSuspectsAndItems and clues[thePersonHere][thingBeingAskedAbout] not in PLACES:
+                knownSuspectsAndItems.append(clues[thePersonHere][thingBeingAskedAbout])
+    
+    input('Press Enter to continue...')
