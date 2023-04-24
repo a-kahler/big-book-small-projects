fc03159e0d73e57f50b33c584d2d0de27ee340f4
a-kahler <austinkahler@proton.me>
2023-04-23 22:25:31 -0400

    initial commit

---
+++ langtonsant.py
@@ -0,0 +1,8 @@
+"""Langton's Ant, by Austii KAY
+A cellular automata animation. Press Ctrl-C to stop.
+More info: https://en.wikipedia.org/wiki/Langton%27s_ant
+
+"""
+
+import copy, random, sys, time
+
